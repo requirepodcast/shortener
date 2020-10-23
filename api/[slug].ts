09aa7224fa64c5fff672e98d1cd9f6c014bfeceb
Adam Siekierski <a@siekierski.ml>
2020-10-23 18:29:15 +0200

    Redirecting function

--- api/[slug].ts
+++ api/[slug].ts
@@ -1,7 +1,22 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+import faunadb from "faunadb";
+
+const secret = process.env.FAUNADB_SECRET;
+const q = faunadb.query;
+const client = new faunadb.Client({ secret });
+
+interface LinkQueryResult {
+  data: {
+    slug: string;
+    destination: string;
+  };
+}
 
 export default (req: NowRequest, res: NowResponse) => {
-  return res.json({
-    message: "hello world!",
-  });
+  return client
+    .query(q.Get(q.Match(q.Index("links"), req.query.slug)))
+    .then((dbs: LinkQueryResult) => {
+      res.redirect(dbs.data.destination);
+    })
+    .catch(() => res.status(404).send("Link not found"));
 };
